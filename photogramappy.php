33a5f5c68588dbb893a5ae49bc57c49946651f26
Aaron Brazell <aaron@technosailor.com>
2019-01-23 11:30:34 -0500

    Init

---
+++ photogramappy.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Photogramappy
+Description: WordPress plugin to allow location tagging of photographs. Requires PHP 7.2 and WordPress 4.9.9+
+Author:      Aaron Brazell
+Version:     2.0
+Author URI:  http://github.com/technosailor
+*/
+
+require_once ABSPATH . '../vendor/autoload.php';
