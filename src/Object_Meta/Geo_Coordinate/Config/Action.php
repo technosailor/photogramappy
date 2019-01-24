da4b4f569f75f239ef717fc542dc538ef5fc1e14
Aaron Brazell <aaron@technosailor.com>
2019-01-24 13:52:31 -0500

    Add view and save to geocoords

--- src/Object_Meta/Geo_Coordinate/Config/Action.php
+++ src/Object_Meta/Geo_Coordinate/Config/Action.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Technosailor\Photogramappy\Object_Meta\Geo_Coordinate\Config;
 
 use MetaboxOrchestra\AdminNotices;
@@ -6,13 +7,33 @@ use MetaboxOrchestra\BoxAction;
 use WP_Post;
 
 class Action implements BoxAction {
-	 private  $post;
+	private $post;
 
-	 public function __construct( WP_Post $post ) {
-	    $this->post = $post;
-	 }
+	public function __construct( WP_Post $post ) {
+		$this->post = $post;
+	}
 
-	 public function save( AdminNotices $notices ): bool {
-		 // TODO: Implement save() method.
-	 }
+	public function save( AdminNotices $notices ): bool {
+
+		if ( ! isset( $_POST[ View::NONCE ] ) ) {
+			return false;
+		}
+
+		if ( ! wp_verify_nonce( $_POST[ View::NONCE ], View::NONCE ) ) {
+			return false;
+		}
+
+		$lat = sanitize_text_field( $_POST[ View::FIELD_LAT ] ) ?? '';
+		$lon = sanitize_text_field( $_POST[ View::FIELD_LONG ] ) ?? '';
+
+		if( ! empty( $lat ) ) {
+			update_post_meta( $this->post->ID, View::FIELD_LAT, $lat );
+		}
+
+		if( ! empty( $lon ) ) {
+			update_post_meta( $this->post->ID, View::FIELD_LONG, $lon );
+		}
+
+		return true;
+	}
 }
--- src/Object_Meta/Geo_Coordinate/Config/View.php
+++ src/Object_Meta/Geo_Coordinate/Config/View.php
@@ -5,6 +5,12 @@ use MetaboxOrchestra\BoxInfo;
 use MetaboxOrchestra\BoxView;
 
 class View implements BoxView {
+
+	const FIELD_LAT = 'latitude';
+	const FIELD_LONG = 'longitude';
+
+	const NONCE = 'geo-coord-nonce';
+
 	private $post;
 
 	public function __construct( \WP_Post $post ) {
@@ -12,6 +18,21 @@ class View implements BoxView {
 	}
 
 	public function render( BoxInfo $info ): string {
-		return 'foo';
+
+		$input = wp_nonce_field( self::NONCE, self::NONCE );
+
+		$input .= sprintf( '<div class="photogramappy-post-meta">%s <input type="text" name="%s" value="%s"></div>',
+			'<strong>' . esc_html__( 'Latitude', 'photogramappy' ) . ':</strong>',
+			esc_attr( self::FIELD_LAT ),
+			esc_attr( get_post_meta( $this->post->ID, self::FIELD_LAT, true ) )
+		);
+
+		$input .= sprintf( '<div class="photogramappy-post-meta">%s <input type="text" name="%s" value="%s"></div>',
+			'<strong>' . esc_html__( 'Longitude', 'photogramappy' ) . ':</strong>',
+			esc_attr( self::FIELD_LONG ),
+			esc_attr( get_post_meta( $this->post->ID, self::FIELD_LONG, true ) )
+		);
+
+		return $input;
 	}
 }
