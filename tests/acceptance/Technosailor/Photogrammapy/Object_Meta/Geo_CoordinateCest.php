f5f22e1fd5c06f2dd8dc2acc76d54708b3d88503
Aaron Brazell <aaron@technosailor.com>
2019-01-24 14:29:49 -0500

    Tweak test

--- tests/acceptance/Technosailor/Photogrammapy/Object_Meta/Geo_CoordinateCest.php
+++ tests/acceptance/Technosailor/Photogrammapy/Object_Meta/Geo_CoordinateCest.php
@@ -26,10 +26,29 @@ class Geo_CoordinateCest {
 
 		$I->amEditingPostWithId( $photograph_id );
 		$I->fillField( '#' . View::FIELD_LAT, '123' );
-		$I->fillField( '#' . View::FIELD_LONG, '-987' );
 		$I->click( '#publish' );
 
 		$I->seeInField( '#' . View::FIELD_LAT, '123' );
+	}
+
+	public function test_that_longitude_is_saved( AcceptanceTester $I ) {
+		$I->am( 'an administrator' );
+		$I->wantTo( 'verify that the Latitude Field can be saved' );
+
+		$I->amOnPage( 'wp-login.php' );
+		$I->wait(2);
+		$I->fillField( '#user_login', 'admin' );
+		$I->fillField( '#user_pass', 'password' );
+		$I->click( '#wp-submit' );
+
+		$photograph_id = $I->havePostInDatabase( [
+			'post_type' => Photographs::NAME
+		] );
+
+		$I->amEditingPostWithId( $photograph_id );
+		$I->fillField( '#' . View::FIELD_LONG, '-987' );
+		$I->click( '#publish' );
+
 		$I->seeInField( '#' . View::FIELD_LONG, '-987' );
 	}
 }
