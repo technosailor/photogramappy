efe9e1c2eca36d216b78fec5516aac1206cd2032
Aaron Brazell <aaron@technosailor.com>
2019-01-26 11:36:12 -0500

    Add the Box namespace

--- src/Object_Meta/Metabox_Object.php
+++ src/Object_Meta/Metabox_Object.php
@@ -2,6 +2,7 @@
 namespace Technosailor\Photogramappy\Object_Meta;
 
 use MetaboxOrchestra\Boxes;
+use Technosailor\Photogramappy\Object_Meta\Geo_Coordinate\Config\Box;
 
 abstract class Metabox_Object {
 	public function register_meta( Boxes $boxes ) {
